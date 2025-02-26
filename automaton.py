fdfc0740b93527ce67ec930292d2daa55833a693
Hang Huynh <hang.huynh523@gmail.com>
2025-02-26 13:08:11 -0500

    Add chromedriver.exe

--- automaton.py
+++ automaton.py
@@ -1,5 +1,5 @@
 from selenium import webdriver # interact with web browser
-from selenium.webdriver.common.keys import keys # simulate key presses
+from selenium.webdriver.common.keys import Keys # simulate key presses
 
 driver = webdriver.Chrome('./chromedriver')
 driver.get("https://navinet.navimedix.com/eligibility-benefits/kchc/search#/")
