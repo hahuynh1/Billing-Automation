f0b7c529596fb04fcfa35fcbd354d1ffdde8df46
Hang Huynh <hang.huynh523@gmail.com>
2025-03-03 13:14:03 -0500

    automate login to navinet

--- automaton.py
+++ automaton.py
@@ -1,7 +1,24 @@
 from selenium import webdriver # interact with web browser
-from selenium.webdriver.common.keys import Keys # simulate key presses
+from selenium.webdriver.common.keys import Keys # simulate key presses\
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import login
 
-driver = webdriver.Chrome('./chromedriver')
-driver.get("https://navinet.navimedix.com/eligibility-benefits/kchc/search#/")
+service = Service('./chromedriver.exe')
+driver = webdriver.Chrome(service=service)
 
-print(driver.title)
+driver.get("https://navinet.navimedix.com/#/")
+
+username = driver.find_element(By.ID, "LoginPortletUsername")
+password = driver.find_element(By.ID, "LoginPortletPassword")
+
+username.send_keys(login.USERNAME)
+password.send_keys(login.PASSWORD)
+
+
+login_button = driver.find_element(By.ID, "submitButton")
+login_button.click()
+
+driver.implicitly_wait(10)
+print(driver.current_url)
+driver.quit()
